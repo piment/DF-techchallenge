5564d28f9bd17ca5c867fd65cf65fa01b51188fa
Romain MURA <romain@mura.io>
2022-05-28 00:07:24 +0200

    CRUD routes for todo created

--- backend/routes/__test__/routes.test.js
+++ backend/routes/__test__/routes.test.js
@@ -3,19 +3,22 @@ const app = require('../../app.js');
 
 describe('Testing API routes', () => {
   it('Should return status code 200 on / get request', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/api');
     expect(res.status).toEqual(200);
   });
 
   it('Should return status code 200 on /todo get request, and an object for response', async () => {
-    const res = await request(app).get('/todo');
+    const res = await request(app).get('/api/todo');
     expect(res.status).toEqual(200);
+    expect(typeof res.body).toEqual('object');
   });
 
   it('Should return status code 201 on /todo post request if todo object is passed', async () => {
     const res = await request(app)
-      .post('/todo')
-      .send({ id: 0, title: 'first todo', desc: 'todo description' });
+      .post('/api/todo')
+      .send({ title: 'first todo', desc: 'todo description' });
     expect(res.status).toEqual(201);
+    expect(typeof res.body).toEqual('object');
+    expect(res.body.id !== undefined).toEqual(true);
   });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import app from './app.js';
+const app = require('./app.js');
 const port = 5000;
 
 app.listen(port, () => {
