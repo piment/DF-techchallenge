e7906637a5a738a5eae373da4b5107f6b9eb995e
piment <romain@mura.io>
2022-06-03 10:12:38 +0200

    adding cors module

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = require('./routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors('https://ata.mura.io'));
 app.use(express.json());
 app.use(router);
 
